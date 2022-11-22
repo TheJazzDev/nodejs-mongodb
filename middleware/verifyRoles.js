1d0de2fa82f0ede9ef4d9b59326e882e1400fc81
TaiwoJazz <babsman4all@gmail.com>
2022-11-22 15:24:50 +0100

    initial commit

---
+++ middleware/verifyRoles.js
@@ -0,0 +1,18 @@
+const verifyRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    const rolesArray = [...allowedRoles];
+
+    if (!req?.roles) {
+      return res.sendStatus(401);
+    }
+
+    const result = req.roles
+      .map((role) => rolesArray.includes(role))
+      .find((val) => val === true);
+
+    if (!result) return res.sendStatus(401);
+    next();
+  };
+};
+
+module.exports = verifyRoles;
